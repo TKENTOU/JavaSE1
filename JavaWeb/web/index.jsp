e31089f5d0eb99762b4dcccf1ef8662f029aefff
TKENTOU <1084367898@qq.com>
2019-07-27 10:58:16 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wjm
+  Date: 2019/7/27
+  Time: 9:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>123</title>
+  </head>
+  <body>
+    <h1 style="color: aquamarine">hello idea</h1>
+  </body>
+</html>
---
+++ src/com/java/HelloWorld.java
@@ -0,0 +1,31 @@
+package com.java;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        ArrayList a = new ArrayList();
+        System.out.println("HelloWorld.main");
+        int b = 10;
+        System.out.println("b = " + b);
+        for (int i = 0; i < 10; i++) {
+
+        }
+
+        for (Object o : a) {
+
+        }
+
+
+    }
+
+
+
+
+    public void a(){
+        System.out.println("a");
+
+    }
+
+}
